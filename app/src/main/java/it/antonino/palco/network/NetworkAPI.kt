50c103d725b7a91700b167e73537189c0b7d6468
thumb80 <an.tonino@virgilio.it>
2023-07-18 22:17:43 +0200

    refactor 3

--- app/src/main/java/it/antonino/palco/network/NetworkAPI.kt
+++ app/src/main/java/it/antonino/palco/network/NetworkAPI.kt
@@ -12,29 +12,29 @@ import retrofit2.http.Path
 
 interface NetworkAPI {
 
-    @Headers("x-api-key:bad777b9-f1f1-49de-bf7b-a10607788108")
+    @Headers("x-api-key:${BuildConfig.AUTH_TOKEN}")
     @GET("getconcerti")
     fun getConcertiNazionali(): Call<ArrayList<Concerto?>?>
 
-    @Headers("x-api-key:bad777b9-f1f1-49de-bf7b-a10607788108")
+    @Headers("x-api-key:${BuildConfig.AUTH_TOKEN}")
     @POST("getconcertimese")
     fun getConcertiNazionaliByMonth(
         @Body dateSearchDTO: DateSearchDTO
     ): Call<ArrayList<Concerto?>?>
 
-    @Headers("x-api-key:bad777b9-f1f1-49de-bf7b-a10607788108")
+    @Headers("x-api-key:${BuildConfig.AUTH_TOKEN}")
     @GET("getconcerticity/{city}")
     fun getConcertiNazionaliByCity(@Path("city") city: String): Call<ArrayList<Concerto?>?>
 
-    @Headers("x-api-key:bad777b9-f1f1-49de-bf7b-a10607788108")
+    @Headers("x-api-key:${BuildConfig.AUTH_TOKEN}")
     @GET("getconcertiartist/{artist}")
     fun getConcertiNazionaliByArtist(@Path("artist") artist: String): Call<ArrayList<Concerto?>?>
 
-    @Headers("x-api-key:bad777b9-f1f1-49de-bf7b-a10607788108")
+    @Headers("x-api-key:${BuildConfig.AUTH_TOKEN}")
     @GET("getcities")
     fun getCities(): Call<ArrayList<String?>?>
 
-    @Headers("x-api-key:bad777b9-f1f1-49de-bf7b-a10607788108")
+    @Headers("x-api-key:${BuildConfig.AUTH_TOKEN}")
     @GET("getartists")
     fun getNationalArtists(): Call<ArrayList<String?>?>
 
