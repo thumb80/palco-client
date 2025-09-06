7955a0124a65421aa1c9fa7018508ae6a7c92c43
thumb80 <an.tonino@virgilio.it>
2025-09-06 10:15:40 +0200

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "it.antonino.palco"
         minSdk = 24
         targetSdk = 36
-        versionCode = 57
+        versionCode = 58
         versionName = "4.20"
 
         buildConfigField("String", "DiscogsApiKey", discogsApiKey)
